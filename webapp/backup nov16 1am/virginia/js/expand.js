540a1beed3c5b68328d569354573e93db37ad3b3
zporges <porges@Zachs-MacBook-Pro.local>
2013-11-20 14:49:34 -0500

    removing unnecessary files

--- webapp/backup nov16 1am/virginia/js/expand.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('.wrapper div').click(function() {
-        $(this).toggleClass('active');
-        $(this).siblings().not(this).toggleClass('hide');
-    });
-});
